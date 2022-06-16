cc3698575949c2cc829738512feb236b943d9ad3
9Akash <105100645+9Akash@users.noreply.github.com>
2022-06-16 19:33:44 +0530

    first pull

--- Random/src/s2.java
+++ Random/src/s2.java
@@ -2,7 +2,7 @@
 public class s2 extends s1{
              //Runtime Polymorphism
 public void m2()  // Method Overriding  
-{String a="Try github";
+{String a="trying git pull for first time";
 {System.out.println(a);}}
 
 public static void main(String[] args) {
